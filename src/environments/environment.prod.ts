7aca68ec125bc85d03532faec44939dc0d123bb3
MarParodi <parodi-2002@hotmail.com>
2026-01-08 23:19:12 -0700

    depl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Esta URL se actualizará con la URL real de Railway después del despliegue
-  apiUrl: 'https://ironplanback-production.up.railway.app'
+  apiUrl: 'https://ironplanback-production.up.railway.app/api'
 };
