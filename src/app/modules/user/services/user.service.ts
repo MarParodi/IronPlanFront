91084ae53e97ccfb923a23d34622ec12105a0a56
MarParodi <parodi-2002@hotmail.com>
2026-01-10 19:18:34 -0700

    rutasprueba

--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -1,26 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class UserService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api/users'; 
+private readonly baseUrl = environment.apiUrl;
 
   // Para actualizar username, birthday, gender
   updateProfile(data: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/profile`, data);
+    return this.http.put(`${this.baseUrl}/users/profile`, data);
   }
 
   // Para subir la foto de perfil a Cloudinary vía Backend
   uploadPhoto(file: File): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
-    return this.http.post(`${this.apiUrl}/profile/photo`, formData);
+    return this.http.post(`${this.baseUrl}/users/profile/photo`, formData);
   }
 
   // Para obtener los datos del usuario logueado (Endpoint /me)
   getMe(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/me`);
+    return this.http.get(`${this.baseUrl}/users/me`);
   }
 }
